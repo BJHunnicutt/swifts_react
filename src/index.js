696b343b80f33a707c51697d5aea2d8aeb9c732e
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2017-09-03 14:59:09 -0700

    set homepage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Styles
+import './styles/index.css';
+import './styles/App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
