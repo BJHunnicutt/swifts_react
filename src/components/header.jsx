085abd7ee4e7842af6c4821acd60291d313fbbd1
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2017-09-03 22:46:09 -0700

    Added Bootstrap and formatted header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-const Header = (props) => {
-  return (
-    <header>
-      <h3>PDX SWIFTS</h3>
-      <ul>
-        <li>
-          <Link to="/">Countdown</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </header>
-  )
+class Header extends Component {
+  handleSelect(eventKey) {
+    // event.preventDefault();
+    console.log(`selected ${eventKey}`);
+  }
+
+  render() {
+    const hamburgerIcon = `<img src="%PUBLIC_URL%/swift.png" >`;
+
+    return (
+      <header>
+        <Nav bsStyle="tabs" onSelect={this.handleSelect}>
+          <NavDropdown eventKey="1" title={<img src="./swift.png" />} id="nav-dropdown" noCaret>
+            <LinkContainer to="/" activeClassName="none">
+              <MenuItem eventKey={1.1} >
+                  Countdown
+              </MenuItem>
+            </LinkContainer>
+
+            <LinkContainer to="/about" activeClassName="none">
+              <MenuItem eventKey={1.2}>
+                About
+              </MenuItem>
+            </LinkContainer>
+
+            <MenuItem divider />
+
+            <LinkContainer to="/contact" activeClassName="none">
+              <MenuItem eventKey={1.3}>
+                Contact
+              </MenuItem>
+            </LinkContainer>
+          </NavDropdown>
+
+          <Navbar.Brand>
+            <a href="/">PDX SWIFTS</a>
+          </Navbar.Brand>
+        </Nav>
+      </header>
+    )
+  }
 };
 
 export default Header;
