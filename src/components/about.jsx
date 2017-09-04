cae2a89d5b1d25040cc3a908d5b218a73cda5966
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2017-09-04 13:57:36 -0700

    Added the bird animation separate from react

---
+++ src/components/about.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import P5Wrapper from '../components/swift_background'
+
+
+class AboutPage extends Component {
+
+
+
+  render() {
+
+    return (
+      <div id="container"></div>
+    )
+  }
+}
+
+export default CountdownPage
